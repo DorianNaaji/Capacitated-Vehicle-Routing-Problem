371a0aba96a1d204888949219ddc66dbfe7f4508
myriam.benali3@gmail.com <myriam.benali3@gmail.com>
2020-05-23 22:29:45 +0200

    Test pour la transformation des solutions

--- tests/TransformateurDeSolutionsTest.java
+++ tests/TransformateurDeSolutionsTest.java
@@ -2,6 +2,10 @@ import algorithms.TransformateurItinéraire;
 import customexceptions.EntrepôtNotFoundException;
 import customexceptions.ListOfClientsIsEmptyException;
 import customexceptions.VehiculeCapacityOutOfBoundsException;
+import gui.CVRPWindow;
+import javafx.application.Application;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 import model.*;
 import org.junit.Assert;
 import org.junit.Test;
@@ -9,96 +13,161 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
-public class TransformateurDeSolutionsTest {
+public class TransformateurDeSolutionsTest extends Application {
 
-    @Test
-    public void testTransformateurDeSolutions() throws VehiculeCapacityOutOfBoundsException, ListOfClientsIsEmptyException, EntrepôtNotFoundException {
+    private Itinéraire itinéraire;
 
-        LinkedList<Client> clients1 = new LinkedList<Client>();
-        Entrepôt entrepôt = new Entrepôt(3,3);
-        Client premierClient1 = new Client(1, 1, 2, 4);
-        Client deuxièmeClient1 = new Client(2, 4, 5, 9);
-        Client troisiemeClient1 = new Client(3, 1, 8, 12);
-        clients1.add(premierClient1);
-        clients1.add(deuxièmeClient1);
-        clients1.add(troisiemeClient1);
-        Itinéraire itinéraire1 = new Itinéraire(clients1, entrepôt);
+    /**
+     * Méthode permettant de charger les données de test
+     * @return une liste d'itinéraires
+     * @throws VehiculeCapacityOutOfBoundsException
+     * @throws ListOfClientsIsEmptyException
+     */
+    public ArrayList<Itinéraire> donnéesDeTest() throws VehiculeCapacityOutOfBoundsException, ListOfClientsIsEmptyException {
 
-        LinkedList<Client> clients2 = new LinkedList<Client>();
-        Client premierClient2 = new Client(1, 3, 4, 5);
-        Client deuxièmeClient2 = new Client(2, 1, 3, 2);
-        Client troisièmeClient2 = new Client(3, 2, 7, 3);
-        clients2.add(premierClient2);
-        clients2.add(deuxièmeClient2);
-        clients2.add(troisièmeClient2);
-        Itinéraire itinéraire2 = new Itinéraire(clients2, entrepôt);
-
-        LinkedList<Client> clients3 = new LinkedList<Client>();
-        Client premierClient3 = new Client(1, 4, 0, 1);
-        Client deuxièmeClient3 = new Client(2, 7, 5, 6);
-        Client troisièmeClient3 = new Client(3, 6, 8, 14);
-        clients3.add(premierClient3);
-        clients3.add(deuxièmeClient3);
-        clients3.add(troisièmeClient3);
-        Itinéraire itinéraire3 = new Itinéraire(clients3, entrepôt);
+        LinkedList<Client> clients = new LinkedList<Client>();
+        Entrepôt entrepôt = new Entrepôt(44,44);
+        Client premierClient = new Client(1, 63, 72, 4);
+        Client deuxièmeClient = new Client(2, 83, 52, 9);
+        Client troisiemeClient = new Client(3, 72, 22, 12);
+        Client quatrièmeClient = new Client(4, 55, 22, 20);
+        clients.add(premierClient);
+        clients.add(deuxièmeClient);
+        clients.add(troisiemeClient);
+        clients.add(quatrièmeClient);
+        this.itinéraire = new Itinéraire(clients, entrepôt);
 
         ArrayList<Itinéraire> itinéraires = new ArrayList<>();
-        itinéraires.add(itinéraire1);
-        itinéraires.add(itinéraire2);
-        itinéraires.add(itinéraire3);
+        itinéraires.add(itinéraire);
 
-        Solution solution = new Solution(itinéraires);
-        // On récupère la taille de l'itinéraire 1 avant la transformation
-        int nbClientsItinéraire1AvantTransformation = itinéraire1.getListeClientsÀLivrer().size();
-        // On récupère la taille de l'itinéraire 2 avant la transformation
-        int nbClientsItinéraire2AvantTransformation = itinéraire2.getListeClientsÀLivrer().size();
-        // On récupère la taille de l'itinéraire 3 avant la transformation
-        int nbClientsItinéraire3AvantTransformation = itinéraire3.getListeClientsÀLivrer().size();
-        // Création d'une liste chaînée contenant les clients à livrer de l'itinéraire 1 avant la transformation
-        LinkedList<Client> clientsItinéraire1AvantTransformation = new LinkedList<>(itinéraire1.getListeClientsÀLivrer());
-        // Création d'une liste chaînée contenant les clients à livrer de l'itinéraire 2 avant la transformation
-        LinkedList<Client> clientsItinéraire2AvantTransformation = new LinkedList<>(itinéraire2.getListeClientsÀLivrer());
-        // Création d'une liste chaînée contenant les clients à livrer de l'itinéraire 3 avant la transformation
-        LinkedList<Client> clientsItinéraire3AvantTransformation = new LinkedList<>(itinéraire3.getListeClientsÀLivrer());
+        return itinéraires;
 
-        /* tests de transformationd'itinéraire dans une solution (transformation locale) */
-        TransformateurItinéraire.transformationÉchange(itinéraire1);
-        // On récupère la taille de l'itinéraire 1 après la transformation locale
-        int nbClientsItinéraire1AprèsTransformation = itinéraire1.getListeClientsÀLivrer().size();
-        // Liste chaînée contenant les clients à livrer de l'itinéraire 1 après la transformation
-        LinkedList<Client> clientsItinéraire1AprèsTransformation = itinéraire1.getListeClientsÀLivrer();
-        //On vérifie si le nombre de clients de l'itinéraire 1 est le même avant et après la transformation
-        Assert.assertEquals(nbClientsItinéraire1AvantTransformation, nbClientsItinéraire1AprèsTransformation);
-        System.out.println("= Test passed (méthode de transformation locale) ✅ Le nombre de client de l'itinéraire 1 reste identique =");
-        //On vérifie si l'itinéraire 1 avant et après la transformation sont différents
-        Assert.assertNotEquals(clientsItinéraire1AvantTransformation, clientsItinéraire1AprèsTransformation);
-        System.out.println("= Test passed (méthode de transformation locale) ✅ L'itinéraire 1 est bien changé =");
+    }
+
+    /**
+     * Méthode pour tester la transformation d'échange entre deux clients d'un itinéraire
+     * @throws VehiculeCapacityOutOfBoundsException
+     * @throws ListOfClientsIsEmptyException
+     * @throws EntrepôtNotFoundException
+     */
+    @Test
+    public void testTransformationÉchangeClients() throws VehiculeCapacityOutOfBoundsException, ListOfClientsIsEmptyException, EntrepôtNotFoundException {
+
+        donnéesDeTest();
+
+        // On récupère la taille de l'itinéraire avant la transformation
+        int nbClientsItinéraireAvantTransformation = this.itinéraire.getListeClientsÀLivrer().size();
+
+        // Création d'une liste chaînée contenant les clients à livrer de l'itinéraire avant la transformation
+        LinkedList<Client> clientsItinéraireAvantTransformation = new LinkedList<>(this.itinéraire.getListeClientsÀLivrer());
+
+        /* tests de transformation d'itinéraire dans une solution (transformation locale) */
+        TransformateurItinéraire.transformationÉchange(this.itinéraire);
+        // On récupère la taille de l'itinéraire après la transformation locale
+        int nbClientsItinéraireAprèsTransformation = this.itinéraire.getListeClientsÀLivrer().size();
+        // Liste chaînée contenant les clients à livrer de l'itinéraire après la transformation
+        LinkedList<Client> clientsItinéraireAprèsTransformation = this.itinéraire.getListeClientsÀLivrer();
+        //On vérifie si le nombre de clients de l'itinéraire est le même avant et après la transformation
+        Assert.assertEquals(nbClientsItinéraireAvantTransformation, nbClientsItinéraireAprèsTransformation);
+        System.out.println("= Test passed (méthode de transformation locale) ✅ Le nombre de client de l'itinéraire reste identique =");
+        //On vérifie si l'itinéraire avant et après la transformation sont différents
+        Assert.assertNotEquals(clientsItinéraireAvantTransformation, clientsItinéraireAprèsTransformation);
+        System.out.println("= Test passed (méthode de transformation locale) ✅ L'itinéraire est bien changé =");
+
+    }
+
+    /**
+     * Méthode pour tester la transformation d'insertion décalage dans un itinéraire
+     * @throws VehiculeCapacityOutOfBoundsException
+     * @throws ListOfClientsIsEmptyException
+     * @throws EntrepôtNotFoundException
+     */
+    @Test
+    public void testInsertionDécalageClients() throws VehiculeCapacityOutOfBoundsException, ListOfClientsIsEmptyException, EntrepôtNotFoundException {
+
+        donnéesDeTest();
+
+        // On récupère la taille de l'itinéraire avant la transformation
+        int nbClientsItinéraireAvantTransformation = this.itinéraire.getListeClientsÀLivrer().size();
+
+        // Création d'une liste chaînée contenant les clients à livrer de l'itinéraire avant la transformation
+        LinkedList<Client> clientsItinéraireAvantTransformation = new LinkedList<>(this.itinéraire.getListeClientsÀLivrer());
 
         /* tests de transformation d'itinéraire dans une solution (insertion décalage) */
-        TransformateurItinéraire.insertionDécalage(itinéraire2);
-        // On récupère la taille de l'itinéraire 2 après la transformation locale
-        int nbClientsItinéraire2AprèsTransformation = itinéraire2.getListeClientsÀLivrer().size();
-        // Liste chaînée contenant les clients à livrer de l'itinéraire 2 après la transformation
-        LinkedList<Client> clientsItinéraire2AprèsTransformation = itinéraire2.getListeClientsÀLivrer();
-        //On vérifie si le nombre de clients de l'itinéraire 2 est le même avant et après la transformation
-        Assert.assertEquals(nbClientsItinéraire2AvantTransformation, nbClientsItinéraire2AprèsTransformation);
-        System.out.println("= Test passed (méthode d'insertion décalage) ✅ Le nombre de client de l'itinéraire 2 reste identique =");
-        //On vérifie si l'itinéraire 2 avant et après la transformation sont différents
-        Assert.assertNotEquals(clientsItinéraire2AvantTransformation, clientsItinéraire2AprèsTransformation);
-        System.out.println("= Test passed (méthode d'insertion décalage) ✅ L'itinéraire 2 est bien changé =");
+        TransformateurItinéraire.insertionDécalage(this.itinéraire);
+        // On récupère la taille de l'itinéraire après la transformation locale
+        int nbClientsItinéraireAprèsTransformation = this.itinéraire.getListeClientsÀLivrer().size();
+        // Liste chaînée contenant les clients à livrer de l'itinéraire après la transformation
+        LinkedList<Client> clientsItinéraireAprèsTransformation = this.itinéraire.getListeClientsÀLivrer();
+        //On vérifie si le nombre de clients de l'itinéraire est le même avant et après la transformation
+        Assert.assertEquals(nbClientsItinéraireAvantTransformation, nbClientsItinéraireAprèsTransformation);
+        System.out.println("= Test passed (méthode d'insertion décalage) ✅ Le nombre de client de l'itinéraire reste identique =");
+        //On vérifie si l'itinéraire avant et après la transformation sont différents
+        Assert.assertNotEquals(clientsItinéraireAvantTransformation, clientsItinéraireAprèsTransformation);
+        System.out.println("= Test passed (méthode d'insertion décalage) ✅ L'itinéraire est bien changé =");
+
+    }
+
+    /**
+     * Méthode pour tester la transformation d'inversion dans un itinéraire
+     * @throws VehiculeCapacityOutOfBoundsException
+     * @throws ListOfClientsIsEmptyException
+     * @throws EntrepôtNotFoundException
+     */
+    @Test
+    public void testInversionClients() throws VehiculeCapacityOutOfBoundsException, ListOfClientsIsEmptyException, EntrepôtNotFoundException {
+
+        donnéesDeTest();
+
+        // On récupère la taille de l'itinéraire avant la transformation
+        int nbClientsItinéraireAvantTransformation = this.itinéraire.getListeClientsÀLivrer().size();
+
+        // Création d'une liste chaînée contenant les clients à livrer de l'itinéraire 3 avant la transformation
+        LinkedList<Client> clientsItinéraireAvantTransformation = new LinkedList<>(this.itinéraire.getListeClientsÀLivrer());
 
         /* tests de transformation de d'itinéraire dans une solution (inversion) */
-        TransformateurItinéraire.inversion(itinéraire3);
-        // On récupère la taille de l'itinéraire 3 après la transformation locale
-        int nbClientsItinéraire3AprèsTransformation = itinéraire3.getListeClientsÀLivrer().size();
-        // Liste chaînée contenant les clients à livrer de l'itinéraire 3 après la transformation
-        LinkedList<Client> clientsItinéraire3AprèsTransformation = itinéraire3.getListeClientsÀLivrer();
-        // On vérifie si le nombre de clients de l'itinéraire 3 est le même avant et après la transformation
-        Assert.assertEquals(nbClientsItinéraire3AvantTransformation, nbClientsItinéraire3AprèsTransformation);
-        System.out.println("= Test passed (méthode d'inversion) ✅ Le nombre de client de l'itinéraire 3 reste identique =");
-        // On vérifie si l'itinéraire 3 avant et après la transformation sont différents
-        Assert.assertNotEquals(clientsItinéraire3AvantTransformation, clientsItinéraire3AprèsTransformation);
-        System.out.println("= Test passed (méthode d'inversion) ✅ L'itinéraire 3 est bien changé =");
+        TransformateurItinéraire.inversion(this.itinéraire);
+        // On récupère la taille de l'itinéraire après la transformation locale
+        int nbClientsItinéraireAprèsTransformation = this.itinéraire.getListeClientsÀLivrer().size();
+        // Liste chaînée contenant les clients à livrer de l'itinéraire après la transformation
+        LinkedList<Client> clientsItinéraireAprèsTransformation = this.itinéraire.getListeClientsÀLivrer();
+        // On vérifie si le nombre de clients de l'itinéraire est le même avant et après la transformation
+        Assert.assertEquals(nbClientsItinéraireAvantTransformation, nbClientsItinéraireAprèsTransformation);
+        System.out.println("= Test passed (méthode d'inversion) ✅ Le nombre de client de l'itinéraire reste identique =");
+        // On vérifie si l'itinéraire avant et après la transformation sont différents
+        Assert.assertNotEquals(clientsItinéraireAvantTransformation, clientsItinéraireAprèsTransformation);
+        System.out.println("= Test passed (méthode d'inversion) ✅ L'itinéraire est bien changé =");
 
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+
+        CVRPWindow mainGui = new CVRPWindow(primaryStage);
+        // affichage de la fenêtre principale
+        mainGui.show();
+        Itinéraire i = donnéesDeTest().get(0);
+        mainGui.getController().drawItinéraire(i, Color.CORAL);
+
+        CVRPWindow secondGui = new CVRPWindow(primaryStage);
+        // affichage de la fenêtre principale
+        secondGui.show();
+        System.out.println();
+        // Décommenter la ligne suivante pour tester la transformation d'échange
+        testTransformationÉchangeClients();
+        // Décommenter la ligne suivante pour tester la transformation d'insertion décalage
+        //testInsertionDécalageClients();
+        // Décommenter la ligne suivante pour tester la transformation d'inversion
+        //testInversionClients();
+        Itinéraire newItinéraire = this.itinéraire;
+        System.out.println();
+        secondGui.getController().drawItinéraire(newItinéraire, Color.CORAL);
+
+    }
+
+    public static void main(String[] args)
+    {
+        launch(args);
     }
 }
